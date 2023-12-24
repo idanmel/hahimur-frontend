03ba9817386247260839c7d56eee10673c6b4cf3
idanmel <idanmel@gmail.com>
2023-12-24 15:13:40 +0200

    fix events

--- src/hahimur_frontend/events.cljs
+++ src/hahimur_frontend/events.cljs
@@ -15,24 +15,24 @@
 (re-frame/reg-event-fx                             ;; note the trailing -fx
  ::fetch-matches                      ;; usage:  (dispatch [:handler-with-http])
  (fn [{:keys [db]} _]                    ;; the first param will be "world"
-   {:db   (assoc db :loading-matches? true)   ;; causes the twirly-waiting-dialog to show??
+   {:db (assoc db :loading-matches? true)   ;; causes the twirly-waiting-dialog to show??
     :http-xhrio {:method          :get
                  :uri             "https://web-production-6d94.up.railway.app/tournaments/1/matches?format=json"
                  :timeout         8000                                           ;; optional see API docs
                  :response-format (ajax/json-response-format {:keywords? true})  ;; IMPORTANT!: You must provide this.
-                 :on-success      [:fetch-matches-success]
-                 :on-failure      [:fetch-matches-error]}}))
+                 :on-success      [::fetch-matches-success]
+                 :on-failure      [::fetch-matches-error]}}))
 
 (re-frame/reg-event-db
  ::fetch-matches-success
- (fn [db [_ {:keys [data]}]]
+ (fn [db [_ {:keys [data]}]] 
    (assoc db 
           :matches data 
-          :loading false)))
+          :loading-matches? false)))
 
 (re-frame/reg-event-db
  ::fetch-matches-error
- (fn [db [_ data]]
+ (fn [db [_ result]]
    (assoc db
-          :matches data
-          :loading false)))
+          :fetch-matches-error result
+          :loading-matches? false)))
